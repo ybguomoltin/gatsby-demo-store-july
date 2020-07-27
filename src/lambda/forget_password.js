ea475386664c90c0dcc28f3f4b5f7295642c465d
ybguomoltin <56880725+ybguomoltin@users.noreply.github.com>
2020-07-27 17:09:59 -0400

    Create forget_password.js

---
+++ src/lambda/forget_password.js
@@ -0,0 +1,59 @@
+const { send } = require("micro");
+const cors = require("micro-cors")();
+const { MoltinClient } = require("@moltin/request");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
+const sendEmail = require("../emails/providers/sendgrid");
+const client = new MoltinClient({
+  client_id: "a2SyrXlQn1HzaH27am93RPzt7hSslNXRYGRy5o5THB",
+  client_secret: "SpYDhGamemK5SjgopkBxQqlyiT3EBCp6Zcf9PmeBmd"
+});
+module.exports = cors(async (req, res) => {
+  const { email = "" } = req.body;
+  try {
+    //look for customer
+    const customerSearch = await client.get(
+      `/customers?filter=eq(email,${email})`
+    );
+
+    console.log(customerSearch);
+    if (customerSearch.data.length === 0)
+      return send(res, 400, { statusCode: 400, error: "customer not found" });
+    let { sk_reset, id, email: useremail } = customerSearch.data[0];
+
+    //if no reset key, generate a new one
+    if (sk_reset === null) {
+      sk_reset = bcrypt.hashSync(useremail + Date.now(), 2);
+      await client.put(`/customers/${id}`, {
+        type: "customer",
+        sk_reset: sk_reset
+      });
+    }
+
+    jwt.sign(
+      { id, useremail },
+      sk_reset,
+      {
+        expiresIn: "1h"
+      },
+      function(err, token_enc) {
+        console.log(token_enc);
+        if (err) send(res, 400, { statusCode: 400, message: err.message });
+        else {
+          sendEmail({
+            to: useremail,
+            subject: "Password Reset Link",
+            body: `Here's your link to reset password: https://epcc-pwa-demo.netlify.app/password_reset/${token_enc}`
+          });
+          send(res, 200, {
+            statusCode: 200,
+            message: "A reset link has been sent."
+          });
+        }
+      }
+    );
+  } catch (e) {
+    send(res, 500, { statusCode: 500, message: "there was a server error" });
+  }
+});
