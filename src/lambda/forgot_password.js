e5a95ff4c7350a7f87de33db0cf7b3c36a767916
ybguomoltin <56880725+ybguomoltin@users.noreply.github.com>
2020-07-28 09:20:39 -0400

    Update and rename forget_password.js to forgot_password.js

--- src/lambda/forgot_password.js
+++ src/lambda/forgot_password.js
@@ -1,4 +1,5 @@
 "use strict";
+
 const { MoltinClient } = require("@moltin/request");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
@@ -9,25 +10,33 @@ const client = new MoltinClient({
 });
 
 exports.handler = async function(event, context, callback) {
-  
-  const { email = "" } = JSON.parse(event.body);
-  
   try {
+    
+    const { email = "" } = JSON.parse(event.body);
+    
+    const headers = {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Methods': 'POST'
+    };
+    
     //look for customer
     const customerSearch = await client.get(
       `/customers?filter=eq(email,${email})`
     );
-
-    console.log(customerSearch);
+    //return 400 if no customer found
     if (customerSearch.data.length === 0)
        return  {
           statusCode: 400,
+          headers,
           body: JSON.stringify({ statusCode: 400, error: "customer not found" })
        }
-    let { sk_reset, id, email: useremail } = customerSearch.data[0];
+    
+    //get email, id and secret key
+    const { sk_reset, id, email: useremail } = customerSearch.data[0];
 
-    //if no reset key, generate a new one
-    if (sk_reset === null) {  
+    //generate a secret key if none exists or is empty string
+    if (sk_reset === null || sk_reset === "") {  
       sk_reset = bcrypt.hashSync(useremail + Date.now(), 2);
       await client.put(`/customers/${id}`, {
         type: "customer",
@@ -35,27 +44,32 @@ exports.handler = async function(event, context, callback) {
       });
     }
     
-     let respBody = {
+    const respBody = {
               statusCode: 200,
+              headers,
               body: JSON.stringify({ statusCode: 200, message: "a link has been sent" })
-     } 
-     const token = jwt.sign({ id, useremail },sk_reset, {expiresIn: "1h"})
-      
-     await sendEmail({
+    }
+    //generate a JWT token, lasts 1 hour
+    const token = jwt.sign({ id, useremail }, sk_reset, {expiresIn: "1h"})
+    
+    //send reset link to customer email
+    await sendEmail({
             to: useremail,
             subject: "Password Reset Link",
             body: `Here's your link to reset password: https://epcc-pwa-demo.netlify.app/password_reset/${token}`
-       });
+    });
 
-     return {
+    return {
               statusCode: 200,
+              headers,
               body: JSON.stringify({ statusCode: 200, message: "a link has been sent" })
-     } 
+    } 
   } catch (e) {
-     return {
+    return {
           statusCode: 500,
+          headers,
           body: JSON.stringify({ statusCode: 500, message: "There was a server error" })
-     }
+    }
   }
 
 };
